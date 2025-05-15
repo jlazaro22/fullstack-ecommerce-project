c85d7f632a253ca3a3616400a48be90652b4d579
Joao Lazaro <joao.lazaro@outlook.com>
2025-05-15 18:29:30 +0100

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { json, Request, Response, urlencoded } from 'express';
 import productsRoutes from './routes/products';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.get('/healthcheck', (req: Request, res: Response) => {
+  res.send('API responding!');
 });
 
 app.use('/products', productsRoutes);
